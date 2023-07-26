d8c6801b9b10d49a92825115e21ad29c765ccac9
Wachi555 <110466235+Wachi555@users.noreply.github.com>
2023-07-26 18:43:00 +0200

    Read Temperatures with Temp struct

--- ArduHeizung.ino
+++ ArduHeizung.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <RTClib.h>
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <avr/wdt.h>
 
+#define PIN_SENSOR 13
+
+
+// Sensors
+OneWire oneWire(PIN_SENSOR);
+DallasTemperature sensors(&oneWire);
+DeviceAddress sens_main; // sens_second;
+
+
+struct Temp { // used as temporary object to get current temperatures
+    float main;
+    Temp() {
+        sensors.requestTemperatures();
+        main = sensors.getTempC(sens_main);
+    }  
+};
+
+
+void setup_sensors() {
+    sensors.begin();
+    sensors.getAddress(sens_main, 0);
+}
+
+
+void setup() {
+    Serial.begin(9600);
+    setup_sensors();
+
+    Serial.println("Setup complete");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    Serial.println(Temp().main);
+    delay(3000);
 }
